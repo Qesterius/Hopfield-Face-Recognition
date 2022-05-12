87cf5cc8fe2ed2eba4a1a7806553dc75f8d407fd
Luki <hexeyxd@gmail.com>
2022-05-12 11:54:12 +0200

    intro

---
+++ main.py
@@ -0,0 +1,5 @@
+from hopfieldnetwork import HopfieldNetwork
+#https://github.com/andreasfelix/hopfieldnetwork?fbclid=IwAR0cO-9uE_S9jOso5VNpSQPUaN7mv0J74XDjgm4NtJNuKjDiwXGUQU1vzVU
+
+
+hopfield_network1 = HopfieldNetwork(N=100)
