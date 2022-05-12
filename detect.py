23645c0a9d2ee7c4a1e8eedb2804931c8bbae98c
Mateusz Wiecek <mateusz.wiecek12345@gmail.com>
2022-05-12 15:49:29 +0200

    camera input

---
+++ detect.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+from hopfieldnetwork import HopfieldNetwork
+import numpy as np
+
+
+def getInput(n):
+    hopfieldnetwork = HopfieldNetwork(filepath="path/to/file")
+    cap = cv. VideoCapture(n)
+
+    while True:
+
+        # Capture the video frame
+        # by frame
+        ret, frame = cap.read()
+
+        # Display the resulting frame
+        cv.imshow('frame', frame)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        ret, thres = cv.threshold(gray, 50, 255, cv.THRESH_BINARY)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        hopfieldnetwork.set_initial_neurons_state(np.copy(np.array(gray)))
+        hopfieldnetwork.update_neurons(iterations=5, mode="async")
+        hopfieldnetwork.compute_energy(hopfieldnetwork.S)
+        cv.imshow('res', hopfieldnetwork.S)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv.destroyAllWindows()
