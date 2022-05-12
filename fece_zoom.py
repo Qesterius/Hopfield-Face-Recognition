91fc9e9eddf2d4214b339aab1dbb765a29758d62
Luki <hexeyxd@gmail.com>
2022-05-12 19:26:53 +0200

    UDUPA

--- fece_zoom.py
+++ fece_zoom.py
@@ -1,7 +1,11 @@
 import cv2
 import cv2 as cv
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 cascPath = "haarcascade_frontalface_default.xml"
+
+
 def getFaces(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faceCascade = cv2.CascadeClassifier(cascPath)
@@ -18,35 +22,48 @@ def getFaces(img):
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)"""
 
+
 def trimPhotoToRect(img, rect):
     x = rect[0]
     y = rect[1]
     w = rect[2]
     h = rect[3]
-    cropped = img[x:x+w,y:y+h]
+    cropped = img[x:x + w, y:y + h]
     return cropped
 
+
 def drawFaceRect(img, rect):
-    x= rect[0]
-    y= rect[1]
-    w= rect[2]
-    h= rect[3]
+    x = rect[0]
+    y = rect[1]
+    w = rect[2]
+    h = rect[3]
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return img
-
+plt.ion()
 
 """ kurwa chcialem stestoewac funkcje powyzsze, ale nie chce mi sie kamerka zalaczyc"""
 cap = cv.VideoCapture(0)
-cv.startWindowThread()
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-while(True):
-    ret,frame = cap.read()
-    if not ret:
-       print("Can't receive frame (stream end?). Exiting ...")
-       exit()
-    img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow("cap",img)
+ret, frame = cap.read()
+img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+screen = plt.imshow(img)
+plt.show()
 
-#cap.release()
+while (True):
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        exit()
+    screen.set_data(cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
+    plt.pause(0.2)
+    plt.draw()
+
+    if cv.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        screen.close()
+        break
+
+
+# cap.release()
