19a22d863d6087e587fc43fbc025e7502d0755ca
Luki <hexeyxd@gmail.com>
2022-05-12 11:56:57 +0200

    intro

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from hopfieldnetwork import HopfieldNetwork
 
 
 hopfield_network1 = HopfieldNetwork(N=100)
+
+#TODO:load images to input pattern
+input_pattern = None
+#train
+hopfield_network1.train_pattern(input_pattern)
+#update neurons
+hopfield_network1.update_neurons(iterations=5, mode="async")
+#Compute the energy function of a pattern:
+hopfield_network1.compute_energy(input_pattern)
+#Save a network as a file:
+hopfield_network1.save_network("path/to/file")
+
+#Open an already trained Hopfield network:
+#hopfield_network2 = HopfieldNetwork(filepath="network2.npz")
