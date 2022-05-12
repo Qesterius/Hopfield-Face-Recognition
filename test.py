609932ce8babdc95f1d0b8bb3b0b3993de507750
Mateusz Wiecek <mateusz.wiecek12345@gmail.com>
2022-05-12 20:04:08 +0200

    .

--- test.py
+++ test.py
@@ -1,59 +1,65 @@
+import hopfieldnetwork
 import numpy as np
 from hopfieldnetwork import *
 from PIL import Image
+import matplotlib.pyplot as plt
 
-def convert_to_int(input):
-    n = len(input)
-    output = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            if input[i, j]: output[i, j] = 1
-            else: output[i, j] = -1
-    return output
 
-n = 100
-N = 100 ** 2
-width = 100
-height = 100
+def convert(x):
+    if x: return 1
+    return -1
+
+
+vconv = np.vectorize(convert)
+
+n = 120
+N = n ** 2
+width = n
+height = n
 
 # grayscale image
 # img = Image.open("data/einstein.jpg").convert('L')
 # img = img.resize((width, height), Image.ANTIALIAS)
 
-input_pattern = Image.open("data/einstein.jpg").convert('L')
-input_pattern = input_pattern.convert("1")
-input_pattern = input_pattern.resize((100, 100))
+# input_pattern = Image.open("data/einstein.jpg")
+# input_pattern = input_pattern.convert("1")
+# input_pattern = input_pattern.resize((100, 100))
 
-input_pattern = np.array(input_pattern)
-# input_pattern = convert_to_int(input_pattern)
-input_pattern = input_pattern.flatten()
+# input_pattern = np.array(input_pattern)
+
+input_pattern1 = image2numpy_array("data/einstein.jpg", (120, 120)).flatten()
+input_pattern2 = image2numpy_array("data/dirac.jpg", (120, 120)).flatten()
+input_pattern3 = image2numpy_array("data/de_broglie.jpg", (120, 120)).flatten()
 
 
-print(input_pattern.shape)
+
 
 
 hopfield_network1 = HopfieldNetwork(N=N)
-print(hopfield_network1.S)
 
-hopfield_network1.train_pattern(input_pattern)
+hopfield_network1.train_pattern([])
 
 half_einstein = np.copy(input_pattern)
 half_einstein[: int(N / 2)] = -1
 
-
 hopfield_network1.set_initial_neurons_state(np.copy(half_einstein))
-data0 = Image.fromarray(np.reshape(input_pattern, (100, 100)), mode='L')
+
+data0 = Image.fromarray(np.reshape(input_pattern, (n, n)), mode='L')
 data0.save("input.png")
 print(hopfield_network1.S)
 
-data0 = Image.fromarray(half_einstein.reshape((100, 100)), mode='L')
-data0.save("half.png")
+# data0 = Image.fromarray(np.reshape(hopfield_network1.S, (n, n)), mode='L')
+# data0.save("inside.png")
+# print(hopfield_network1.S)
+
+# data0 = Image.fromarray(half_einstein.reshape((n, n)), mode='L')
+# data0.save("half.png")
 
 hopfield_network1.update_neurons(iterations=5, mode="async")
 
 print(hopfield_network1.S)
 print(np.sum(hopfield_network1.S))
 
-data = Image.fromarray(np.reshape(hopfield_network1.S, (100, 100)), mode='L')
+data = Image.fromarray(np.reshape(hopfield_network1.S, (n, n)), mode='L')
 
 data.save("result.png")
